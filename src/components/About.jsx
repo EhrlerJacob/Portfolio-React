e2ca00744138758e3ab4487dd3073855479b6ca7
Jacob Ehrler <jacobehrler@Jacobs-MBP.lan>
2023-09-13 15:41:00 -0400

    made quick changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,7 +16,7 @@ const About = () => {
             visible: { opacity: 1, x: 0 },
           }}>
             <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Let Me Infroduce Myself</p>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Let Me Introduce Myself</p>
             </div>
             <p className='text-xl mt-20'>
                 My name is Jacob Ehrler and I am a full-stack developer. I am committed writing
